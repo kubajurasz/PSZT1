f334c96be05caa61fa650c67047ce8bd69732847
Kuba Jurasz <kuba.jurasz.1@gmail.com>
2020-11-15 14:00:05 +0100

    Almost acceptable

--- main.py
+++ main.py
@@ -16,33 +16,42 @@ class Evolution:
 
     def __init__(self, cities, population_size):
         self.cities = cities
-        self.population = [Specimen(len(cities)) for i in range(population_size)]
+        self.population = [Specimen(len(cities), [random.choices([0, 1], [0.7, 0.3])[0] for i in range(len(cities))]) for i in range(population_size)]
+        self.next_generation = []
 
     def rank_population(self, r):
         for i in self.population:
             i.check_quality(self.cities, r)
-            self.population.sort(key=operator.attrgetter('quality'))
+        self.population.sort(key=operator.attrgetter('quality'))
 
     def evolve(self, number_of_generations, r):
         self.rank_population(r)
         for i in range(number_of_generations):
-            for j in range(len(self.population)):
-                print(j, self.population[j].quality)
-            for j in range(10, 39):
-                self.population[j].mutation()
+            self.next_generation = []
+            for _ in range(10):
+                self.next_generation += self.population[random.randint(0, 9)].cross(self.population[random.randint(0, 9)])
 
-            for k in range(25, 39):
-                self.population[k].cross(self.population[k - 15])
+            for x in self.next_generation:
+                x.mutation_2()
+                x.check_quality(self.cities, r)
+                # x.mutation(1/74)
+
+            self.next_generation.sort(key=operator.attrgetter('quality'))
+            self.population = self.population[0:10] + self.next_generation[0:10]
             self.rank_population(r)
-            print("---------------dupsko-------------------")
+
+            print("---------------new top 7---------------")
+            for x in range(7):
+                print(self.population[x].quality)
+
         return self.population[0]
 
 
 class Specimen:
-    def __init__(self, size):
+    def __init__(self, size, hospitals):
         self.size = size
         self.quality = 999
-        self.hospitals = [random.choices([0, 1], [0.8, 0.2])[0] for i in range(size)]
+        self.hospitals = hospitals
 
     def check_quality(self, cities, r):
         img_x = Image.open('map4.png')
@@ -64,22 +73,22 @@ class Specimen:
         else:
             self.quality = 999
 
-    def mutation(self):
+    def mutation(self, p):
+        for i in self.hospitals:
+            if random.random() > p:
+                i = (i + 1) % 2
+
+    def mutation_2(self):
         mutating_gene = random.randint(0, 73)
-        if random.randint(0, 1) == 1:
-            while self.hospitals[mutating_gene] == 0:
-                mutating_gene = random.randint(0, 73)
-        else:
-            while self.hospitals[mutating_gene] == 1:
-                mutating_gene = random.randint(0, 73)
+        while self.hospitals[mutating_gene] == 0:
+            mutating_gene = random.randint(0, 73)
         self.hospitals[mutating_gene] = (self.hospitals[mutating_gene] + 1) % 2
 
     def cross(self, other):
         where = random.randint(0, 73)
         gene1 = self.hospitals[where:] + other.hospitals[:where]
         gene2 = other.hospitals[where:] + self.hospitals[:where]
-        self.hospitals = gene1
-        other.hospitals = gene2
+        return [Specimen(self.size, gene1), Specimen(self.size, gene2)]
 
 
 class Cities:
@@ -114,8 +123,8 @@ def main():
     start_time = time.time()
     cities_with_hospitals = []
 
-    evolution = Evolution(cities.cities, 40)
-    winner = evolution.evolve(30, 187)
+    evolution = Evolution(cities.cities, 20)
+    winner = evolution.evolve(100, 187)
 
     for i in range(len(winner.hospitals)):
         if winner.hospitals[i] == 1:
